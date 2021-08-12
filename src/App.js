907943de4006d2a607056bb7f6aadfa45115cb9a
Geradot1980 <geradot80@gmail.com>
2021-08-12 14:26:54 +0300

    Lesson №15 End

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
-// import Footer from './Footer';
-// import Header from './Header';
-// import Technologies from './Technologies';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+
 
 
 const App = () => {
