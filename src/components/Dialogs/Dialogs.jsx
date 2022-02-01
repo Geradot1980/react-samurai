bcca47f9d665bde9d3b839c9cccc6c6032bd3476
Geradot1980 <geradot80@gmail.com>
2022-02-01 22:20:45 +0300

    lesson29HW end

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
+//
 const DialogItem = (props) => {
-	return <div className={s.dialogPerson}>
+	return <div className={s.person}><img src="https://avatars.mds.yandex.net/i?id=e59182938582bce69289cfa854f5b0e4-5850122-images-thumbs&n=13&exp=1" alt="" srcset="" className={s.avatar} /><div className={s.dialogPerson}>
 		<NavLink to={`/dialogs/${props.userid}`} activeClassName={s.active}>{props.user}</NavLink>
 	</div>
+	</div>
 }
-const MessageItem = (props) => { return <div className={s.message}>{props.message}</div> }
 
+//
+const MessageItem = (props) => {
+	return <div className={s.messagesFlex}>
+		<img src="https://avatars.mds.yandex.net/i?id=2a0000017a0cf85aad348ffba92753b35099-3106771-images-thumbs&n=13&exp=1" alt="" srcset="" className={s.messageAvatar} />
+		<div className={s.message}>{props.message}</div></div>
+}
+
+//
 const Dialogs = (props) => {
-	console.log(`Dialogs - ${props}`);
 
 	let dialogElements = props.state.dialogsData.map(dialog => <DialogItem user={dialog.name} userid={dialog.id} />)
 	let messageElements = props.state.messagesData.map(dialog => <MessageItem message={dialog.message} />)
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,6 +1,9 @@
 import s from './Post.module.css'
 
 const Post = (props) => {
-	return <div className={s.item}>{`${props.massage}  Likes ${props.likes}`}</div>
+	return <div className={s.message}>
+		<div><img src="http://cs622426.vk.me/v622426834/409d/baLqspYwi84.jpg" alt="Аватар" className={s.avatar} /></div>
+		<div className={s.item}>{`${props.massage}  Likes ${props.likes}`}</div>
+	</div>
 }
 export default Post;
