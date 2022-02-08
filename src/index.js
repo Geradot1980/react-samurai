e885ec3ab8185db493f9703c1c8296bf38c732f0
Geradot1980 <geradot80@gmail.com>
2022-02-08 07:59:24 +0300

    state2store end

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './index.css';
-import state from './redux/state';
+import store from './redux/state';
 //import { rerenderEntireTree } from './render';
 import reportWebVitals from './reportWebVitals';
-import { addPost, changeNewPostText, subscribe } from './redux/state';
+//import { addPost, changeNewPostText, subscribe } from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import React from 'react';
@@ -11,14 +11,14 @@ import ReactDOM from 'react-dom';
 const rerenderEntireTree = (state) => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App state={state} addPost={addPost} changeNewPostText={changeNewPostText} />
+			<App state={state} addPost={store.addPost.bind(store)} changeNewPostText={store.changeNewPostText.bind(store)} />
 		</BrowserRouter>,
 		document.getElementById('root'));
 }
 
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,56 @@
 //import { rerenderEntireTree } from "../render";
+let store = {
+	_state: {
+		profilePage: {
+			postsData: [
+				{ massage: '1Hi, how are you?', likes: 15 },
+				{ massage: '2It\'s my first post.', likes: 115 },
+				{ massage: '3And What?', likes: 75 },
+				{ massage: 'I am good?', likes: 53 }
+			],
+			myPostsCurrentText: "Main text1"
+		},
+		messagesPage: {
+			messagesData: [
+				{ id: 1, message: "Mes1" },
+				{ id: 2, message: "Mes2-Geradot1" },
+				{ id: 3, message: "Mes3-Geradot2" },
+				{ id: 4, message: "Mes4Geradot3" },
+				{ id: 5, message: "5Geradot4" }
+			],
+			dialogsData: [
+				{ id: 1324, name: "Geradot" },
+				{ id: 98123, name: "Geradot1" },
+				{ id: 415657, name: "Geradot2" },
+				{ id: 71864, name: "Geradot3" },
+				{ id: 4561, name: "Geradot4" }]
+		}
+	},
+	getState() {
+		return this._state;
+	},
+	_callSubscriber() { console.log("first"); },
+	addPost() {
+		debugger;
+		let newPost = {
+			massage: this._state.profilePage.myPostsCurrentText,
+			likes: 0
+		};
+		this._state.profilePage.postsData.push(newPost);
+		this._state.profilePage.myPostsCurrentText = '';
+		this._callSubscriber(this._state);
+	},
+	changeNewPostText(NewPostText) {
+		this._state.profilePage.myPostsCurrentText = NewPostText;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
+	}
+}
 
-let state = {
+
+/* let state = {
 	profilePage: {
 		postsData: [
 			{ massage: '1Hi, how are you?', likes: 15 },
@@ -26,7 +76,7 @@ let state = {
 			{ id: 4561, name: "Geradot4" }
 		]
 	}
-}
+} */
 
 /* export let addPost = (postMessage) => {
 	let newPost = {
@@ -37,9 +87,9 @@ let state = {
 	rerenderEntireTree(state);
 }; */
 
-let rerenderEntireTree;
+//let rerenderEntireTree;
 
-export const addPost = () => {
+/* export const addPost = () => {
 	let newPost = {
 		massage: state.profilePage.myPostsCurrentText,
 		likes: 0
@@ -47,16 +97,17 @@ export const addPost = () => {
 	state.profilePage.postsData.push(newPost);
 	state.profilePage.myPostsCurrentText = '';
 	rerenderEntireTree(state);
-};
+}; */
 
-export const changeNewPostText = (NewPostText) => {
+/* export const changeNewPostText = (NewPostText) => {
 	state.profilePage.myPostsCurrentText = NewPostText;
 	rerenderEntireTree(state);
-};
+}; */
 
-export const subscribe = (observer) => {
+/* export const subscribe = (observer) => {
 	rerenderEntireTree = observer;
-}
+} */
 
-export default state;
+export default store;
+window.store = store;
 
